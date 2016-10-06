a88b906f599703e46f1f32f15d2792b5036639e7
= <Jmitchell1991@gmail.com>
2016-10-06 12:07:47 -0700

    [setup] - sets up initial server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParse = require('body-parser');
+
+app.use(bodyParse.json({limit: '50mb'}));
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  //insert ensureSchema funciton here
+  console.log('we are now listening on ', app.get('port'))
+})
