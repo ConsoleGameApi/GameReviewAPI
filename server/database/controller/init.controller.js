a698bb85244e701cf3b6546903546ea77bf3fe8f
= <Jmitchell1991@gmail.com>
2016-10-07 17:19:43 -0700

    [feature] - adds Promise.all array and returns the id's of game, genre and platform insertions

--- server/database/controller/init.controller.js
+++ server/database/controller/init.controller.js
@@ -3,6 +3,7 @@ const data = require('../data/ign');
 const gameController = require('./game.controller.js');
 const genreController = require('./genre.controller.js');
 const platformController = require('./platform.controller.js');
+const Promise = require('bluebird');
 
 const dataFiltered = data.filter((game) => {
   return (game.genre.length > 1)
@@ -10,33 +11,21 @@ const dataFiltered = data.filter((game) => {
 
 const gameData = dataFiltered.slice(0, 100);
 const initializeDatabase = () => {
-  gameController.game.add(gameData[15])
-  .then((resultOfInit) => {
-    console.log('resultOfInit: ', resultOfInit)
-  });
+
  
+  const promiseArray = []
+  promiseArray.push(gameController.game.add(gameData[20]));
+  promiseArray.push(genreController.genre.add(gameData[20]));
+  promiseArray.push(platformController.platform.add(gameData[20]));
 
+  Promise.all(promiseArray)
+  .then((dataArray) => {
+    console.log('returned dataArray: ', dataArray)
+    console.log('game id: ', dataArray[0])
+    console.log('genre id: ', dataArray[1])
+    console.log('platform id: ', dataArray[2])
+  });
 
-
-  // genre.add below returns a promise
-  // genreController.genre.add(gameData[13])
-  // .then((resultOfInit) => {
-  //   console.log('resultOfInit: ', resultOfInit)
-  // });
-
-  
-
-
-
-
-  //platform.add below returns a promise
-  
-
-
-  // platformController.platform.add(gameData[12])
-  // .then((resultOfInit) => {
-  //   console.log('resultOfInit: ', resultOfInit)
-  // })
 
 
 
