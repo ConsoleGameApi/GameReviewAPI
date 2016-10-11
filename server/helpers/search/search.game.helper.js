78404c8fb830b3ee97c2b77c8df69fa64b02a7bb
= <Jmitchell1991@gmail.com>
2016-10-10 23:32:36 -0700

    [refactor] - refactors code cleans up for linter

--- server/helpers/search/search.game.helper.js
+++ server/helpers/search/search.game.helper.js
@@ -1,32 +1,9 @@
 const searchGameModel = require('../../models/search/search.game.model.js');
 
-const searchGameHelper = module.exports
+const searchGameHelper = module.exports;
 
 
 searchGameHelper.read = (params) => {
-  return searchGameModel.find(params)
-}
+  return searchGameModel.find(params);
+};
 
-// searchGameHelper.read = (params) => {
-//   console.log('inside searchGameHelper params are: ', params)
-//   if (params.score || params.score_range) {
-//     return searchGameHelper.scores(params);
-//   } else if (params.title) {
-//     return searchGameModel.title(params);
-//   }
-// };
-
-
-// searchGameHelper.scores = (params) => {
-//   if (params.score) {
-//     if (params.release_year) {
-//       return searchGameModel.score_release_year(params);
-//     }
-//     return searchGameModel.score(params);
-//   } else if (params.score_range) {
-//     if (params.release_year) {
-//       return searchGameModel.score_release_year_range(params);
-//     }
-//     return searchGameModel.score_range(params);
-//   }
-// };
