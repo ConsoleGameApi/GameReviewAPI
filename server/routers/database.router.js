7a8680eb5ee6f9bcff1229c3e85a2dba5c495697
= <Jmitchell1991@gmail.com>
2016-10-12 11:50:29 -0700

    [deploy] - removes dotenv

--- server/routers/database.router.js
+++ server/routers/database.router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const path = require('path');
+// path.join(__dirname, '/database/seeding/init.controller');
 const initSeedingController = require('../database/seeding/init.controller')
 
 
